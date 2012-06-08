77541fc791bc5f3922d93648d13a22ec74ad7a3d
Sam Beam <sbeam@onsetcorps.net>
2012-06-08 09:30:11 -0400

    rename admin order details override

--- app/overrides/admin_order_form_line_item_row.rb
+++ app/overrides/admin_order_form_line_item_row.rb
@@ -1,6 +1,6 @@
 Deface::Override.new(:virtual_path => "spree/admin/shared/_order_details",
-                     :name => "admin_order_form_line_item_row",
-                     :replace => "tr[data-hook=order_details_line_item_row] td:first-child", #[data-hook=admin_order_form_line_item_row]", # replace the first <td> in the row only?
-                     :partial => "spree/admin/admin_order_form_line_item_row",
+                     :name => "admin_order_details_line_item_row",
+                     :replace => "tr[data-hook=order_details_line_item_row] td:first-child", # replace the first <td> in the row only
+                     :partial => "spree/admin/order_details_line_item_row",
                      :disabled => false)
 
